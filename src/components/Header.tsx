ed8d7740aef34225b0afedb93ccbe8a286ed177e
swostika61 <swostikalamichhane61@gmail.com>
2022-04-01 22:06:41 +0545

    style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <div className="w-full flex items-center bg-teal-800 h-12">
+    <div className="w-full flex items-center bg-teal-800 sm:h-12">
       <h1 className="font-normal text-xl text-white ml-4 leading-10 capitalize">Note using react typescript and CSS tailwind</h1>
     </div>
   );
